0683f70922b50d3999442443db03c16c7efc894f
irene-lotus <irene@lotusflare.com>
2016-08-24 11:56:41 -0700

    resubmit

--- app/src/main/java/com/mintminter/simplewidgetdemo/MainActivity.java
+++ app/src/main/java/com/mintminter/simplewidgetdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.mintminter.simplewidgetdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.mintminter.simplewidget.SimpleProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SimpleProgressBar progressBar = (SimpleProgressBar) findViewById(R.id.simpleprobressbar);
+        progressBar.setProgress(0.6f);
+
+        SimpleProgressBar progressBarWithGap = (SimpleProgressBar) findViewById(R.id.simpleprobressbar_with_gap);
+        progressBarWithGap.setProgress(0.4f);
+
+
     }
 }
