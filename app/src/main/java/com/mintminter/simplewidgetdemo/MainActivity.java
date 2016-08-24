2b28549320325aa81d6dace78b19b86eabdf898b
irene-lotus <irene@lotusflare.com>
2016-08-24 11:15:20 -0700

    Revert "Add demo screenshot"
    This reverts commit 5b2a7594d72f5570750faa16e49671ec363b7abd.

--- app/src/main/java/com/mintminter/simplewidgetdemo/MainActivity.java
+++ app/src/main/java/com/mintminter/simplewidgetdemo/MainActivity.java
@@ -2,9 +2,6 @@ package com.mintminter.simplewidgetdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ProgressBar;
-
-import com.mintminter.simplewidget.SimpleProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SimpleProgressBar progressBar = (SimpleProgressBar) findViewById(R.id.simpleprobressbar);
-        progressBar.setProgress(0.6f);
-
-        SimpleProgressBar progressBarWithGap = (SimpleProgressBar) findViewById(R.id.simpleprobressbar_with_gap);
-        progressBarWithGap.setProgress(0.4f);
-
-
     }
 }
